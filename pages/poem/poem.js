d86acd04d74810a7335b58abdc60b342bc22c08b
LampV <1071952403@qq.com>
2020-05-30 22:29:44 +0800

    update poem page css

--- pages/poem/poem.js
+++ pages/poem/poem.js
@@ -2,39 +2,75 @@
 const app = getApp()
 
 Page({
-    data: {
-        iconArray: ['']
-    },
+  data: {
+    iconArray: [''],
+    showBottomModal: true,
+    infoType: 'author',
+    fullAuthor:false,
+  },
 
-    onLoad: function (options) {
-        let remoteUrl = app.globalData.remoteIp + '/poem/text'
-        wx.request({
-            url: remoteUrl,
-            method: 'POST',
-            data: {
-                poem_id: options.id
-            },
-            success: res => {
-                this.setData({
-                    poem: {
-                        title: res.data.title,
-                        author_name: res.data.author_name,
-                        author_dynasty: res.data.dynasty,
-                        sentences: res.data.text.split('\n'),
-                        author_abstract_parags: res.data.author_abstract.split('\n'),
-                    }
-                })
-                console.log(res.data)
-            }
-        })
+  onLoad: function(options) {
+    let remoteUrl = app.globalData.remoteIp + '/poem/text'
+    wx.request({
+      url: remoteUrl,
+      method: 'POST',
+      data: {
+        poem_id: options.id
+      },
+      success: res => {
         this.setData({
-            id: options.id,
-            currentItemId: "poem_content",
+          poem: {
+            title: res.data.title,
+            author_name: res.data.author_name,
+            author_dynasty: res.data.dynasty,
+            sentences: res.data.text.split('\n'),
+            author_abstract_parags: res.data.author_abstract.split('\n'),
+            author_abstract: res.data.author_abstract,
+          }
         })
-    },
-    swipeToAuthor: function () {
-        this.setData({
-            currentItemId: 'poem_author'
-        })
-    },
+        console.log(res.data)
+      }
+    })
+    this.setData({
+      id: options.id,
+      currentItemId: "poem_content",
+    })
+  },
+  swipeToAuthor: function() {
+    this.setData({
+      currentItemId: 'poem_author'
+    })
+  },
+  ChangeBottomModal() {
+    this.setData({
+      showBottomModal: ! this.data.showBottomModal
+    })
+  },
+  InfoTypeChange(e){
+    console.log(e)
+    let type = e.target.dataset.type
+    this.setData({
+      infoType: type
+    })
+  },
+  showFullAuthor(){
+    this.setData({
+      fullAuthor: true
+    })
+  },
+  learnPoem(){
+    wx.showToast({
+      title: '学习，施工中',
+    })
+  },
+  likePoem(){
+    wx.showToast({
+      title: '收藏，施工中',
+    })
+  },
+  forwardPoem(){
+    wx.showToast({
+      title: '转发，施工中',
+    })
+  }
 })
